e7e4e9fc2e2cb8921b2878ac95e1c026113379af
Viktor <Viktor.przeczek@gmail.com>
2025-05-08 11:26:06 +0200

    Create app and dev stack

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "cz.przeczaandsons.isat",
+  appName: "isat",
+  webDir: "dist",
+};
+
+export default config;
